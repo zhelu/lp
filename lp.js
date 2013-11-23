7a8fdff1d919c345a904ffca208c4a0d1c0d4271
Zhe Lu <zhelumail@gmail.com>
2013-11-23 00:08:32 -0500

    Lines and object working. Just need algorithm.

--- lp.js
+++ lp.js
@@ -2,13 +2,15 @@
   var width = 700;
   var height = 400;
   var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
+  var keyAlreadyDown = false;
+  var deleteLine = false;
+  var _clickX, _clickY;
+  var dataset = [];
+  var objective = [{x : width / 2, y : height / 2, a : 0, b : - height / 10}];
 
-  var dataset = [{
-    a : 10,
-    b : 10,
-    x : 100,
-    y : 50
-  }];
+  function innerProduct(p1, p2) {
+    return p1.a * p2.a + p1.b * p2.b;
+  }
 
   function getX1(d) {
     if (d.b == 0) {
@@ -66,8 +68,34 @@
     return (d.x * d.a + d.y * d.b - getX2(d) * d.a) / d.b;
   }
 
+  function keydown() {
+    if (keyAlreadyDown) {
+      return false;
+    }
+    keyAlreadyDown = true;
+    if (d3.event.keyCode == 16) {
+      // shift
+      d3.event.preventDefault();
+      deleteLine = true;
+    }
+    return svg;
+  }
+  
+  function keyup() {
+    keyAlreadyDown = false;
+    deleteLine = false;
+    return svg;
+  }
+  
+  function mousedown() {
+    d3.event.preventDefault();
+    _clickX = d3.mouse(this)[0];
+    _clickY = d3.mouse(this)[1];
+    return svg;
+  }
 
   svg.append("defs").append("marker").attr("id", "Triangle").attr("viewBox", "0 0 10 10").attr("refX", "0").attr("refY", "5").attr("markerUnits", "strokeWidth").attr("markerWidth", "10").attr("markerHeight", "10").attr("orient", "auto").append("path").attr("d", "M 0 0 L 10 5 L 0 10 z").attr("fill", "rgba(128,128,128,0.4)");
+  svg.select("defs").append("marker").attr("id", "Triangle2").attr("viewBox", "0 0 10 10").attr("refX", "0").attr("refY", "5").attr("markerUnits", "strokeWidth").attr("markerWidth", "5").attr("markerHeight", "5").attr("orient", "auto").append("path").attr("d", "M 0 0 L 10 5 L 0 10 z").attr("fill", "rgba(135,206,235,0.6)");
 
   function dragmovebase(d) {
     d3.event.sourceEvent.stopPropagation
@@ -86,80 +114,126 @@
   }
 
   function dragstarted(d) {
-    d3.event.sourceEvent.stopPropagation
+    d3.event.sourceEvent.stopPropagation;
+    if (deleteLine) {
+      var p = d3.select(this).datum();
+      for (var i = 0; i < dataset.length; i++) {
+        if (dataset[i] == p) {
+          dataset.splice(i,1);
+          return;
+        }
+      }
+    }
+    d3.select(this).classed("dragging", true);
+  }
+  
+  function dragstartedObjective(d) {
+    d3.event.sourceEvent.stopPropagation;
+    d3.select(this).classed("dragging", true);
   }
 
   function dragended(d) {
+    d3.select(this).classed("dragging", false);
     update();
   }
 
   function click() {
-    if (d3.event.defaultPrevented) {
+    if (d3.event.defaultPrevented || deleteLine) {
       return;
     }
+    var _a = d3.mouse(this)[0] - _clickX;
+    var _b = d3.mouse(this)[1] - _clickY;
+    if (_a == 0 && _b == 0) {
+      _a = 20;
+      _b = 20;
+    }
     dataset.push({
-      x : d3.mouse(this)[0],
-      y : d3.mouse(this)[1],
-      a : 10,
-      b : 10
+      x : _clickX,
+      y : _clickY,
+      a : _a,
+      b : _b,
     });
     update();
   }
 
   var dragbase = d3.behavior.drag().on("drag", dragmovebase).on("dragstart", dragstarted).on("dragend", dragended);
   var dragtip = d3.behavior.drag().on("drag", dragmovetip).on("dragstart", dragstarted).on("dragend", dragended);
+  var dragObjectiveBase = d3.behavior.drag().on("drag", dragmovebase).on("dragstart", dragstartedObjective).on("dragend", dragended);
+  var dragObjectiveTip = d3.behavior.drag().on("drag", dragmovetip).on("dragstart", dragstartedObjective).on("dragend", dragended);
+
+  function updateLineColor(obj) {
+    return obj.classed("upper", function (d) {return (innerProduct (d, objective[0]) > 0);}).classed("lower", function (d) {return (innerProduct (d, objective[0]) < 0);});
+  }
+
+  function updateLine(obj) {
+    return updateLineColor(obj.attr("x1", getX1).attr("x2", getX2).attr("y1", getY1).attr("y2", getY2));
+  }
+  
+  function updatePointer(obj) {
+    return obj.attr("x1", function(d) {
+      return d.x;
+    }).attr("y1", function(d) {
+      return d.y;
+    }).attr("x2", function(d) {
+      return d.x + d.a;
+    }).attr("y2", function(d) {
+      return d.y + d.b;
+    });
+  }
+  
+  function updateBase(obj, r) {
+    var r_ = r;
+    return obj.attr("cx", function(d) {
+      return d.x;
+    }).attr("cy", function(d) {
+      return d.y;
+    }).attr("r", r_)
+  }
+  
+  function updateTip(obj, r) {
+    var r_ = r;
+    return obj.attr("cx", function(d) {
+      return d.x + d.a;
+    }).attr("cy", function(d) {
+      return d.y + d.b;
+    }).attr("r", r_);
+  }
 
   function update() {
-    var lines = svg.selectAll("line.line").data(dataset).attr("x1", getX1).attr("x2", getX2).attr("y1", getY1).attr("y2", getY2);
-    var basePts = svg.selectAll("circle.base").data(dataset).attr("cx", function(d) {
-      return d.x;
-    }).attr("cy", function(d) {
-      return d.y;
-    });
-    var tipPts = svg.selectAll("circle.tip").data(dataset).attr("cx", function(d) {
-      return d.x + d.a;
-    }).attr("cy", function(d) {
-      return d.y + d.b;
-    });
-    var pointers = svg.selectAll("line.pointer").data(dataset).attr("x1", function(d) {
-      return d.x;
-    }).attr("y1", function(d) {
-      return d.y;
-    }).attr("x2", function(d) {
-      return d.x + d.a;
-    }).attr("y2", function(d) {
-      return d.y + d.b;
-    });
+    var objectiveText = svg.selectAll("text.objective").data(objective).attr("x", function (d) { return d.x;}).attr("y", function (d) { return d.y;});
+    objectiveText.enter().append("text").attr("x", function (d) { return d.x;}).attr("y", function (d) { return d.y;}).text("Objective").classed("objective", true);
+    objectiveText.exit().remove();
+    
+    // data set
+    var lines = updateLine(svg.selectAll("line.line").data(dataset));
+    var basePts = updateBase(svg.selectAll("circle.base").data(dataset), 4);
+    var pointers = updatePointer(svg.selectAll("line.pointer").data(dataset));
+    var tipPts = updateTip(svg.selectAll("circle.tip").data(dataset), 4);
 
-    lines.enter().append("line").attr("x1", getX1).attr("x2", getX2).attr("y1", getY1).attr("y2", getY2).attr("stroke", "black").attr("class", "line");
-    basePts.enter().append("circle").attr("cx", function(d) {
-      return d.x;
-    }).attr("cy", function(d) {
-      return d.y;
-    }).attr("r", 3).attr("class", "base").call(dragbase);
-    tipPts.enter().append("circle").attr("cx", function(d) {
-      return d.x + d.a;
-    }).attr("cy", function(d) {
-      return d.y + d.b;
-    }).attr("r", 3).attr("class", "tip").call(dragtip);
-    pointers.enter().append("line").attr("x1", function(d) {
-      return d.x;
-    }).attr("y1", function(d) {
-      return d.y;
-    }).attr("x2", function(d) {
-      return d.x + d.a;
-    }).attr("y2", function(d) {
-      return d.y + d.b;
-    }).attr("stroke", "black").attr("class", "pointer").attr("marker-end", "url(#Triangle)");
+    updateLine(lines.enter().append("line")).classed("line", true);
+    updateBase(basePts.enter().append("circle"),4).classed("base", true).call(dragbase);
+    updatePointer(pointers.enter().append("line")).classed("pointer", true).attr("marker-end", "url(#Triangle)");
+    updateTip(tipPts.enter().append("circle"),4).classed("tip", true).call(dragtip);
 
     lines.exit().remove();
     basePts.exit().remove();
     tipPts.exit().remove();
     pointers.exit().remove();
+    
+    // objective
+    var objectiveLine = updatePointer(svg.selectAll("line.objective").data(objective));
+    updatePointer(objectiveLine.enter().append("line")).classed("objective", true).attr("marker-end", "url(#Triangle2)");
+    var objectiveBase = updateBase(svg.selectAll("circle.objectiveBase").data(objective), 5);
+    updateBase(objectiveBase.enter().append("circle"), 5).classed("objectiveBase", true).call(dragObjectiveBase);
+    var objectiveTip = updateTip(svg.selectAll("circle.objectiveTip").data(objective), 5);
+    updateTip(objectiveTip.enter().append("circle"), 5).classed("objectiveTip", true).call(dragObjectiveTip);
   }
 
   update();
 
-  svg.on("click", click);
+  svg.on("click", click).on("mousedown", mousedown);
+  d3.select(window).on("keyup", keyup).on("keydown", keydown);
+  
+  d3.select("#buttonStart").on("click", function() { dataset = []; update();});
 
 }).call(this);
